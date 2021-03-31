7045477f81fe07f338b6e8bda48baf785c142562
Marcus <marcus.daley1@outlook.com>
2021-03-30 23:33:12 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = "Marcus Daley";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const firstInitial = currentUser[0];
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
+
